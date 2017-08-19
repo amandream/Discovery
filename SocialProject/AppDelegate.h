9616e6ddade759a5f61e8e9a4c6a5523c5a9c2b6
mac <mac@macdembp-2.fios-router.home>
2017-08-19 09:53:43 -0400

    Initial Commit

---
+++ SocialProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialProject
+//
+//  Created by mac on 2017/8/19.
+//  Copyright © 2017年 msw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialProject
+//
+//  Created by mac on 2017/8/19.
+//  Copyright © 2017年 msw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
